715951bd57f5322571f857526e128c4a693a16db
Anthony Gavazzi <18180363+Gavazzi1@users.noreply.github.com>
2019-02-08 11:50:49 -0800

    Added batch script

---
+++ _NewFile.bat
@@ -0,0 +1,27 @@
+@echo off
+
+set fn=%date:~-4,4%-%date:~-10,2%-%date:~7,2%.txt
+
+for /f %%a in ('wmic path win32_localtime get dayofweek /format:list ^| findstr "="') do (set %%a)
+
+echo Todo: >> %fn%
+
+if %DayOfWeek% == 5 (
+  echo.>> %fn%
+  echo Planview timesheet >> %fn%)
+
+echo.>> %fn%
+echo.>> %fn%
+
+
+echo Done: >> %fn%
+echo.>> %fn%
+echo.>> %fn%
+
+
+echo Notes: >> %fn%
+echo.>> %fn%
+echo in  @ >> %fn%
+echo out @ >> %fn%
+echo in  @ >> %fn%
+echo out @ >> %fn%
